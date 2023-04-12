f06bfa2dacee9de61a587ba581e2cddd51bf7b7f
Calvin <calvinscheck@gmail.com>
2023-04-12 11:34:32 +0200

    Create a route to exxecute the seed file

--- app/posts/routes.py
+++ app/posts/routes.py
@@ -29,6 +29,7 @@ def blog_post(slug):
     post = Post.query.filter_by(slug=slug).first_or_404()
     return render_template('Blog/blog_posts.html', post=post)
 
+# Creating the route where users can submit their own posts
 @blueprint.route('/publish-posts', methods=['GET', 'POST'])
 def publish_post():
     if request.method == 'POST':
@@ -78,3 +79,12 @@ def delete_post(post_id):
     db.session.delete(post)
     db.session.commit()
     return redirect(url_for('simple_pages.index'))
+
+# Workaround solution to execute the seed file in order to create a database for the hosted webpage
+@blueprint.route('/run-seed')
+def run_seed():
+  if not Post.query.filter_by(slug='author2').first():
+    import app.scripts.seed
+    return 'Database seed completed!'
+  else:
+    return 'Nothing to run.'
